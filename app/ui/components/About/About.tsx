5dc93280a930495ea84df72f0945910855a02d64
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-08-13 18:35:24 +0300

    updated fonts

--- app/ui/components/About/About.tsx
+++ app/ui/components/About/About.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import React from 'react'
-import { suezOne } from '../../fonts/fonts'
 
 const About = () => {
     return (
--- app/ui/fonts/fonts.ts
+++ app/ui/fonts/fonts.ts
@@ -1,9 +1,4 @@
-import { Suez_One, Open_Sans } from "next/font/google";
-
-// export const suezOne = Suez_One({
-//     subsets: ["hebrew"],
-//     weight: "400"
-// });
+import { Open_Sans } from "next/font/google";
 
 export const openSans = Open_Sans({
     subsets: ["hebrew"],
