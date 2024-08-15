78b4f8121576f5ed32a434e891ab837f683bbc51
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-08-15 13:43:41 +0300

    FAQ

--- app/(view)/faq/page.tsx
+++ app/(view)/faq/page.tsx
@@ -7,6 +7,7 @@ const Page = () => {
     <section className='elementWidth faq' id="faq">
       <div className='faq__info'>
         <h2>שאלות נפוצות</h2>
+        <p>כאן תמצאו תשובות לשאלות ששואלים אותנו לעיתים קרובות. אם לא מצאתם תשובה לשאלה שלכם, אל תהססו ליצור קשר - נשמח לעזור!</p>
         <div className='faq__quest'>
           {FAQquestion.map((q, idx) => (
             <QItem key={idx} faq={q} />
--- app/ui/components/NavBar/navigationLinks.ts
+++ app/ui/components/NavBar/navigationLinks.ts
@@ -1,9 +1,9 @@
 import { Navigation } from "./navigationTypes";
 
 export const navigationLinks: Array<Navigation> = [
-    { link: "/", title: "פרויקטים" },
+    { link: "/#projects", title: "פרויקטים" },
+    { link: "/#feedbacks", title: "פידבקים" },
     { link: "/about", title: "אודות" },
-    { link: "/", title: "פידבקים" },
     { link: "/faq", title: "שאלות נפוצות" },
     { link: "/blog", title: "בלוג" },
 ]
