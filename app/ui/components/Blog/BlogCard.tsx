d944d1db91b90b9d30e1f84424fa190263967900
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-08-18 03:29:41 +0300

    blog card

--- app/ui/components/Blog/BlogCard.tsx
+++ app/ui/components/Blog/BlogCard.tsx
@@ -17,7 +17,7 @@ const BlogCard = ({post}:any) => {
                 <Link href={`/blog/${post.title.replaceAll(' ', '-')}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center border-purple-800 border-2 text-purple-800 bg-slate-50 rounded-lg hover:bg-violet-100 hover:text-violet-700 focus:ring-2 focus:outline-none focus:bg-violet-100">
                     המשך קריאה
                     <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                     </svg>
                 </Link>
             </div>
