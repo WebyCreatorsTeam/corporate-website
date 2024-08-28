8714088df080e0509aa63bce211206577a5adf4d
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-08-28 13:25:17 +0300

    post card skeleton fix

--- app/ui/Skeletons/Blog/BlogCardSkeleton.tsx
+++ app/ui/Skeletons/Blog/BlogCardSkeleton.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BlogCardSkeleton = () => {
     return (
         <article className="max-w-sm bg-white border border-gray-300 rounded-lg shadow animate-pulse">
-            <div className="w-[384px] h-[215px] bg-slate-300 rounded-t-lg"></div>
+            <div className="w-[322px] sm:w-[382px] h-[215px] bg-slate-300 rounded-t-lg"></div>
             <div className="p-5">
                 <p className="w-[130px] mb-3 text-sm font-normal text-gray-700 h-4 bg-slate-300 rounded"></p>
                 <h5 className="mb-4 mt-6 text-2xl font-bold tracking-tight text-gray-900 h-8 bg-slate-300 rounded"></h5>
