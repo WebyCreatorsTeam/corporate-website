0d9fbe8c0021b722b67448272f1f4e84f72910b9
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-08-28 15:07:18 +0300

    metadata of blog page

--- app/(view)/blog/page.tsx
+++ app/(view)/blog/page.tsx
@@ -6,28 +6,43 @@ import React, { Suspense } from 'react'
 
 export const dynamic = "force-dynamic"
 
-export const metadata: Metadata = {
-    title: "בלוג",
-    description: "משתפים אתכם בתובנות והמידע שאספנו לאורך הזמן, בבלוג של Weby נחקור את הכלים השונים ונעזור לכם להבין כיצד ליישם את הידע בפרויקטים שלכם.",
-    metadataBase: new URL("https://www.weby.team"),
-
-    openGraph: {
+export const generateMetadata = (): Metadata => {
+    return {
         title: "בלוג",
-        description: "משתפים אתכם בתובנות והמידע שאספנו לאורך הזמן, בבלוג של Weby נחקור את הכלים השונים ונעזור לכם להבין כיצד ליישם את הידע בפרויקטים שלכם.",
-        url: "https://www.weby.team/blog",
-        type: 'website',
-        siteName: 'Weby Team',
-        images: [{
-            url: "https://www.weby.team/logo_white_bkgr.png",
-            width: 730,
-            height: 483,
-            alt: "לוגו של וובי"
-        }],
-        emails: ['webycreatorsteam@gmail.com'],
-        phoneNumbers: ['972-51-5183320'],
-        countryName: 'Israel',
-    },
+        description: "משתפים אתכם בתובנות והמידע שאספנו לאורך הזמן, בבלוג נחקור את הכלים השונים ונעזור לכם להבין כיצד ליישם את הידע בפרויקטים שלכם.",
+
+        openGraph: {
+            title: "בלוג",
+            description: "משתפים אתכם בתובנות והמידע שאספנו לאורך הזמן, בבלוג נחקור את הכלים השונים ונעזור לכם להבין כיצד ליישם את הידע בפרויקטים שלכם.",    
+        }
+    }
 }
+
+
+
+
+// export const metadata: Metadata = {
+//     title: "בלוג",
+//     description: "משתפים אתכם בתובנות והמידע שאספנו לאורך הזמן, בבלוג של Weby נחקור את הכלים השונים ונעזור לכם להבין כיצד ליישם את הידע בפרויקטים שלכם.",
+//     metadataBase: new URL("https://www.weby.team"),
+
+//     openGraph: {
+//         title: "בלוג",
+//         description: "משתפים אתכם בתובנות והמידע שאספנו לאורך הזמן, בבלוג של Weby נחקור את הכלים השונים ונעזור לכם להבין כיצד ליישם את הידע בפרויקטים שלכם.",
+//         url: "https://www.weby.team/blog",
+//         type: 'website',
+//         siteName: 'Weby Team',
+//         images: [{
+//             url: "https://www.weby.team/logo_white_bkgr.png",
+//             width: 730,
+//             height: 483,
+//             alt: "לוגו של וובי"
+//         }],
+//         emails: ['webycreatorsteam@gmail.com'],
+//         phoneNumbers: ['972-51-5183320'],
+//         countryName: 'Israel',
+//     },
+// }
 
 const Page = async () => {
     return (
