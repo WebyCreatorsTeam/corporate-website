34ef016fbca565c1297a07b9d67b0cff1716eae5
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-09-16 17:54:16 +0300

    post check

--- app/lib/data/blog/blog.data.ts
+++ app/lib/data/blog/blog.data.ts
@@ -18,33 +18,33 @@ export async function getBlog() {
     }
 }
 
-export async function getOnePost(postName: string) {
-    try {
-        // console.log(first)
-        await connectToDatabase();
-        console.log(`line 25 postName ${postName}`)
-        const title = decodeURI(postName);
-        console.log(`line 27 title ${title}`)
-        const regex = new RegExp(title.replaceAll("-", " ").replaceAll('%3A', ':').replaceAll('%2C', ","), 'i')
-        const post = await Post.findOne({ title: { $regex: regex } }).select(['-draft'])
-        return post;
-    } catch (error) {
-        console.log(`error getOnePost`)
-        console.log(error);
-        return error
-        // return {}
-    }
-}
+// export async function getOnePost(postName: string) {
+//     try {
+//         // console.log(first)
+//         await connectToDatabase();
+//         console.log(`line 25 postName ${postName}`)
+//         const title = decodeURI(postName);
+//         console.log(`line 27 title ${title}`)
+//         const regex = new RegExp(title.replaceAll("-", " ").replaceAll('%3A', ':').replaceAll('%2C', ","), 'i')
+//         const post = await Post.findOne({ title: { $regex: regex } }).select(['-draft'])
+//         return post;
+//     } catch (error) {
+//         console.log(`error getOnePost`)
+//         console.log(error);
+//         return error
+//         // return {}
+//     }
+// }
 
-export async function getTitlePost(postId: string) {
-    try {
-        await connectToDatabase();
-        const post = await Post.findById(postId).select(['title'])
-        console.log(`line 42 post ${post}`)
-        return post.title;
-    } catch (error) {
-        console.log(`error getTitlePost`)
-        console.log(error);
-        return ""
-    }
-}
+// export async function getTitlePost(postId: string) {
+//     try {
+//         await connectToDatabase();
+//         const post = await Post.findById(postId).select(['title'])
+//         console.log(`line 42 post ${post}`)
+//         return post.title;
+//     } catch (error) {
+//         console.log(`error getTitlePost`)
+//         console.log(error);
+//         return ""
+//     }
+// }
