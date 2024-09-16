91eeb4c577ec04d035ec5ed2e0be8d3c5f497d6e
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-09-16 20:34:57 +0300

    Update page.tsx

--- app/(view)/blog/[postName]/page.tsx
+++ app/(view)/blog/[postName]/page.tsx
@@ -54,7 +54,9 @@ interface IPagePost {
 const Page: FC<IPagePost> = async ({ params }) => {
   const { postName } = params;
   console.log(postName)
-  const uri = await decodeURIComponent(postName).replaceAll("-", " ");
+  const uri = decodeURIComponent(decodeURIComponent(postName)).replaceAll("-", " ")
+
+  // const uri = await decodeURIComponent(postName).replaceAll("-", " ");
   const post = await getOnePost(uri)
 
   // let post
